b6b2bbd3bc00546623d9abe07c539bd59889822e
Katarzyna Napierala <k_napierala@op.pl>
2022-01-24 08:49:16 +0100

    months in separated divs, firstDayOfWeek corrected

--- script.js
+++ script.js
@@ -1,54 +1,60 @@
-const days = ["pon", "wt", "śrd", "czw", "pt", "sb", "niedz"];
-let firstDayOfWeek = 0;
+{
+    const days = ["pon", "wt", "śrd", "czw", "pt", "sb", "niedz"];
+    let firstDayOfWeek = 0;
 
-const createDay = (dayName) => {
-    const day = document.createElement('div');
-    day.classList.add("day");
-    day.innerText = dayName;
+    const createDay = (dayName) => {
+        const day = document.createElement('div');
+        day.classList.add("day");
+        day.innerText = dayName;
 
-    return day;
-};
+        return day;
+    };
 
-const createWeek = (countDays, firstDayOfWeek) => {
-    const week = document.createElement('div');
+    const createWeek = (countDays, firstDayOfWeek) => {
+        const week = document.createElement('div');
 
-    for (let i = 0; i < countDays; i++) {
-        week.appendChild(createDay(days[firstDayOfWeek]));
+        for (let i = 0; i < countDays; i++) {
+            week.appendChild(createDay(days[firstDayOfWeek]));
 
-        firstDayOfWeek++
+            firstDayOfWeek++
+            if (firstDayOfWeek >= days.length) {
+                firstDayOfWeek = 0;
+            };
+        };
+
+        return week;
+    };
+
+    function createMonth(daysOfMonth) {
+        const month = document.createElement('div');
+
+        const weekLength = days.length;
+        let countDays = daysOfMonth;
+
+        for (let i = 0; i < Math.ceil(daysOfMonth / weekLength); i++) {
+            if (countDays >= weekLength) {
+                month.appendChild(createWeek(weekLength, firstDayOfWeek));
+                countDays -= weekLength;
+            } else {
+                month.appendChild(createWeek(countDays, firstDayOfWeek));
+            };
+        };
+
+        firstDayOfWeek += daysOfMonth % weekLength
+        
         if (firstDayOfWeek >= days.length) {
-            firstDayOfWeek = 0
+            firstDayOfWeek -= weekLength;
+        };
+
+        return month;
+    };
+
+    const createYear = () => {
+        for (let i = 0; i < 12; i++) {
+            document.querySelector(".container").innerHTML += `<div class="day day__month">${i + 1}</div>`;
+            document.querySelector(".container").appendChild(createMonth(30));
         };
     };
 
-    return week;
+    createYear();
 };
-
-function createMonth(daysOfMonth) {
-    const weekLength = days.length
-    let countDays = daysOfMonth;
-
-    for (let i = 0; i < Math.ceil(daysOfMonth / weekLength); i++) {
-        if (countDays >= weekLength) {
-            document.querySelector(".container").appendChild(createWeek(weekLength, firstDayOfWeek));
-            countDays -= weekLength;
-        } else {
-            document.querySelector(".container").appendChild(createWeek(countDays, firstDayOfWeek));
-        };
-    };
-
-    firstDayOfWeek += daysOfMonth%weekLength
-    if (firstDayOfWeek >= days.length) {
-        firstDayOfWeek = 0
-    };
-    
-};
-
-const createYear = () => {
-    for (let i = 0; i < 12; i++) {
-        document.querySelector(".container").innerHTML += `<div class="day day__month">${i + 1}</div>`;
-        createMonth(30);
-    };
-};
-
-createYear();
